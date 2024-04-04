109d571b9065e8fcc325eb5953af14b294c98f70
hamzaiqbal10 <20hi4@queensu.ca>
2024-04-04 01:11:42 -0400

    setting up connection with phpmyadmin

--- db_connection.php
+++ db_connection.php
@@ -1,1 +1,20 @@
 <!-- Handles the database connection using PDO (this can be included in your other scripts). -->
+<!--This link is for managing SQL database
+    http://localhost/phpmyadmin -->
+
+
+<?php
+$servername = "localhost";
+$username = "root"; // default XAMPP username
+$password = ""; // default XAMPP password is empty
+$dbname = "rentalDB";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
